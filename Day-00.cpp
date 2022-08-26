f61fabdbdcce6500ba5f26f5fefa132c89d9ba91
KEERTHANA-E <ekeerthana2020@gmail.com>
2022-08-27 00:09:16 +0530

    Day-01 problem completed

---
+++ Day-00.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+  ll n;
+  cin >> n;
+  cout << n;
+  return 0;
+}
--- Day-01.cpp
+++ Day-01.cpp
@@ -1,12 +1,38 @@
-#include<stdio.h>
-#include<bits/stdc++.h>
-using namespace std;
-using ll = long long;
-
-int main()
+/*
+leetCoode-https://leetcode.com/problems/two-sum/submissions/
+GOOGLE
+hashmap approach
+*/
+class Solution
 {
-  ll n;
-  cin >> n;
-  cout << n;
-  return 0;
-}
+public:
+  vector<int> twoSum(vector<int> &nums, int target)
+  {
+    int i, k;
+    map<int, vector<int>> mp;
+    for (i = 0; i < nums.size(); i++)
+    {
+      mp[nums[i]].push_back(i);
+    }
+    for (i = 0; i < nums.size(); i++)
+    {
+      if (mp.find(target - nums[i]) != mp.end())
+      {
+        if (nums[i] == target - nums[i])
+        {
+          if (mp[nums[i]].size() > 1)
+          {
+            k = mp[nums[i]][1];
+            break;
+          }
+        }
+        else
+        {
+          k = mp[target - nums[i]][0];
+          break;
+        }
+      }
+    }
+    return {i, k};
+  }
+};
